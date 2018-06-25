0f1498c1668c0a5f8420e0dbde14ee2331a75463
Erin Black <erin.black@fieldnation.com>
2018-06-25 14:23:56 -0500

    first use of map function

--- example.js
+++ example.js
@@ -1,1 +1,20 @@
-console.log('array map method lecture')
+console.log('array map method lecture');
+
+const caterpillar = 'Ben';
+const caterpillarToButterfly = function(caterpillar){
+    return {name: caterpillar, isButterfly: true}
+}
+
+const caterpillarArray = [
+    'Ben',
+    'Hanz',
+    'Kristy',
+    'Candace'
+];
+
+const butterflyArray = caterpillarArray.map(value => {
+    const newButterfly =  caterpillarToButterfly(value);
+    return newButterfly;
+} );
+
+console.log('butterflyArray', butterflyArray)
